605e3a778a2a37a57c5e25956ddc0a4eb4e75b6e
Joan_E <joanevans18@gmail.com>
2020-10-01 12:41:01 +0300

    -

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -6,14 +6,14 @@ import 'rxjs/add/operator/map';
 @Injectable()
 export class ProductService {
   private _albumUrl:string = '../assets/album.json'
-  
-  constructor( private _http: Http) { 
-  }
-  
   getAlbum(id:number){
     return this._http.get(this._albumUrl).map(response => {
       return response.json()
          })
   }
+  constructor( private _http: Http) { 
+  }
+
+  
 
 }
