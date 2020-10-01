511b49772d1dc7a6ea47f7c373beaba28cfd186a
Joan_E <joanevans18@gmail.com>
2020-10-01 22:50:28 +0300

    Created Interfaces to describe data

--- src/app/product.service.ts
+++ src/app/product.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
+import {Album} from './album'
 import { Http, Response } from '@angular/http';
 import 'rxjs/add/operator/map';
+import { Observable } from 'rxjs/Observable';
 
 
 @Injectable()
@@ -9,8 +11,8 @@ export class ProductService {
   
   constructor( private _http: Http) { }
 
-  getAlbum(id: number){
-    return this._http.get(this._albumUrl).map((response) => response.json())
+  getAlbum(id: number):Observable<Album>{
+    return this._http.get(this._albumUrl).map((response) => <Album>response.json())
   }
   
 
